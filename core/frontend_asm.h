4862d4fe11f46b7c950c91dffb3e8df3b764125c
figurantpp <renan.r.marcelino2@protonmail.com>
2020-12-26 02:27:04 -0300

    Changed assembly macros to inline functions

--- core/frontend_asm.h
+++ core/frontend_asm.h
@@ -5,64 +5,75 @@
 #ifndef BFC_FRONTEND_ASM_H
 #define BFC_FRONTEND_ASM_H
 
-#define bfc_asm_increase_rsi_content(file) ({fputs("inc byte [rsi]\n", file);})
-#define bfc_asm_add_rsi_content(file, count) ({fprintf(file, "add byte [rsi], %zu\n", count);})
+static inline void bfc_asm_increase_rsi_content(FILE *file)
+{ fputs("inc byte [rsi]\n", file); }
 
-#define bfc_asm_decrease_rsi_content(file) ({fputs("dec byte [rsi]\n", file);})
-#define bfc_asm_subtract_rsi_content(file, count) ({fprintf(file, "sub byte [rsi], %zu\n", count);})
+static inline void bfc_asm_add_rsi_content(FILE *file, size_t count)
+{ fprintf(file, "add byte [rsi], %zu\n", count); }
 
-#define bfc_asm_increase_rsi(file) ({fprintf(file, "inc rsi\n");})
-#define bfc_asm_add_rsi(file, count) ({fprintf(file, "add rsi, %zu\n", count);})
+static inline void bfc_asm_decrease_rsi_content(FILE *file)
+{ fputs("dec byte [rsi]\n", file); }
 
-#define bfc_asm_decrease_rsi(file) ({fputs("dec rsi\n", file);})
-#define bfc_asm_subtract_rsi(file, count) ({fprintf(file, "sub rsi, %zu\n", count);})
+static inline void bfc_asm_subtract_rsi_content(FILE *file, size_t count)
+{ fprintf(file, "sub byte [rsi], %zu\n", count); }
 
+static inline void bfc_asm_increase_rsi(FILE *file)
+{ fprintf(file, "inc rsi\n"); }
 
-#define bfc_asm_write(file) \
-    ({                      \
-        fprintf(file, \
-            "mov rax, 1\n" \
-            "syscall\n"); \
-    })
+static inline void bfc_asm_add_rsi(FILE *file, size_t count)
+{ fprintf(file, "add rsi, %zu\n", count); }
 
-#define bfc_asm_read(file) \
-    ({                      \
-        fprintf(file, \
-            "mov rax, 0\n" \
-            "syscall\n"); \
-    })
+static inline void bfc_asm_decrease_rsi(FILE *file)
+{ fputs("dec rsi\n", file); }
+
+static inline void bfc_asm_subtract_rsi(FILE *file, size_t count)
+{ fprintf(file, "sub rsi, %zu\n", count); }
 
 
-#define bfc_asm_jump_if_zero(file, location) \
-({                                           \
-                                             \
-    fprintf( \
-    file, \
-    "" \
-    "cmp byte [rsi], 0\n" \
-    "je %s\n", \
-    location\
-    ); \
-\
-})
+static inline void bfc_asm_write(FILE *file)
+{
+    fprintf(file,
+            "mov rax, 1\n"
+            "syscall\n");
+}
 
-#define bfc_asm_jump_if_not_zero(file, location) \
-({                                           \
-                                             \
-    fprintf( \
-    file, \
-    "" \
-    "cmp byte [rsi], 0\n" \
-    "jne %s\n", \
-    location\
-    ); \
-\
-})
+static inline void bfc_asm_read(FILE *file)
+{
+    fprintf(file,
+            "mov rax, 0\n"
+            "syscall\n");
+}
 
 
-#define bfc_asm_set_label(file, label_name) \
-({                                          \
- fprintf(file, "%s:\n", label_name);                                           \
-})
+static inline void bfc_asm_jump_if_zero(FILE *file, const char *location)
+{
+
+    fprintf(
+            file,
+            ""
+            "cmp byte [rsi], 0\n"
+            "je %s\n",
+            location
+    );
+
+}
+
+static inline void bfc_asm_jump_if_not_zero(FILE *file, const char *location)
+{
+
+    fprintf(
+            file,
+            ""
+            "cmp byte [rsi], 0\n"
+            "jne %s\n",
+            location
+    );
+}
+
+
+static inline void bfc_asm_set_label(FILE *file, const char *label_name)
+{
+    fprintf(file, "%s:\n", label_name);
+}
 
 #endif //BFC_FRONTEND_ASM_H
